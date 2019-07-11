8a135752360b3cbbb81feca111a2f9d2772a6319
Craig Franklin <craigjfranklin@gmail.com>
2019-07-11 17:06:00 +1000

    Actually try logging in before deploying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,4 @@
+gcloud auth activate-service-account --key-file ./.gcloud/keyfile.json
+
 gcloud builds submit --config cloudbuild.yaml ./afl_data
 gcloud beta run deploy ${SERVICE_NAME} --image gcr.io/${PROJECT_ID}/afl_data --memory 2Gi
