305e62a00a16154afaa5d3aec7e7e42f6155d524
Craig Franklin <craigjfranklin@gmail.com>
2019-07-11 17:49:25 +1000

    Add region flag to gcloud deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,2 @@
-gcloud auth activate-service-account --key-file ${HOME}/.gcloud/keyfile.json
-
 gcloud builds submit --config cloudbuild.yaml ./afl_data
-gcloud beta run deploy ${SERVICE_NAME} --image gcr.io/${PROJECT_ID}/afl_data --memory 2Gi
+gcloud beta run deploy ${SERVICE_NAME} --image gcr.io/${PROJECT_ID}/afl_data --memory 2Gi --region us-central1
