cae40984b5808458aba36287a586774ced755d30
Craig Franklin <craigjfranklin@gmail.com>
2019-07-11 17:28:11 +1000

    Simplify keyfile handling

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
-ls .gcloud
-gcloud auth activate-service-account --key-file ./.gcloud/keyfile.json
+gcloud auth activate-service-account --key-file ${HOME}/.gcloud/keyfile.json
 
 gcloud builds submit --config cloudbuild.yaml ./afl_data
 gcloud beta run deploy ${SERVICE_NAME} --image gcr.io/${PROJECT_ID}/afl_data --memory 2Gi
