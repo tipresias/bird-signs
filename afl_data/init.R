92815ae893d1d90013b0a3a3865e2e484401cfea
Craig Franklin <craigjfranklin@gmail.com>
2019-07-12 11:29:41 +1000

    Make deploy script executable

--- afl_data/init.R
+++ afl_data/init.R
@@ -1,42 +1,20 @@
-# Suppressing all messages during installation, because R is excessive,
-# and it overloads the CI logs
+install.packages("devtools", quiet = TRUE, verbose = FALSE)
 
-# store a copy of system2
-assign("system2.default", base::system2, baseenv())
-
-# create a quiet version of system2
-assign(
-  "system2.quiet",
-  function(...)system2.default(..., stdout = FALSE, stderr = FALSE),
-  baseenv()
-)
-
-# overwrite system2 with the quiet version
-assignInNamespace("system2", system2.quiet, "base")
-
-# this is now message-free:
-res <- eval(suppressMessages(install_github('ROAUth', 'duncantl')))
-
-install.packages("devtools")
-
-install.packages("BH")
-install.packages("dplyr")
-install.packages("plogr")
-install.packages("plumber")
-install.packages("progress")
-install.packages("purrr")
-install.packages("rvest")
-install.packages("stringr")
+install.packages("BH", quiet = TRUE, verbose = FALSE)
+install.packages("dplyr", quiet = TRUE, verbose = FALSE)
+install.packages("plogr", quiet = TRUE, verbose = FALSE)
+install.packages("plumber", quiet = TRUE, verbose = FALSE)
+install.packages("progress", quiet = TRUE, verbose = FALSE)
+install.packages("purrr", quiet = TRUE, verbose = FALSE)
+install.packages("rvest", quiet = TRUE, verbose = FALSE)
+install.packages("stringr", quiet = TRUE, verbose = FALSE)
 
 # Installing via git rather than github to avoid unauthenticated API
 # rate limits in CI
-devtools::install_git("git://github.com/jimmyday12/fitzRoy.git")
+devtools::install_git("git://github.com/jimmyday12/fitzRoy.git", quiet = TRUE)
 # Only using master-branch install to get new pivot_wider function.
 # Can switch back to CRAN once that gets released
-devtools::install_git("git://github.com/tidyverse/tidyr.git")
+devtools::install_git("git://github.com/tidyverse/tidyr.git", quiet = TRUE)
 
-install.packages("roxygen2")
-install.packages("testthat")
-
-# reset system2 to its original version
-assignInNamespace("system2", system2.default, "base")
+install.packages("roxygen2", quiet = TRUE, verbose = FALSE)
+install.packages("testthat", quiet = TRUE, verbose = FALSE)
