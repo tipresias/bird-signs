c7389098531098d38833f26a8d3477d088ba7895
Craig Franklin <craigjfranklin@gmail.com>
2019-09-29 10:23:12 +1000

    Export travis_wait for use in script file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,15 @@
 
 set -euo pipefail
 
-gcloud builds submit --config cloudbuild.yaml ./afl_data
+# We need travis_wait for Travis CI builds, because installing R packages takes forever.
+# On a local machine, we can just build & deploy as normal.
+if [ -n "$(LC_ALL=C type -t travis_wait)" ] && [ "$(LC_ALL=C type -t travis_wait)" = function ]
+  then
+    travis_wait 30 gcloud builds submit --config cloudbuild.yaml ./afl_data
+  else
+    gcloud builds submit --config cloudbuild.yaml ./afl_data
+fi
+
 gcloud beta run deploy ${SERVICE_NAME} \
   --image gcr.io/${PROJECT_ID}/afl_data \
   --memory 2Gi \
