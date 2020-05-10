8073407300d28177314dc1d4df8c65835e021a72
Craig Franklin <craigjfranklin@gmail.com>
2020-05-10 11:48:54 +1000

    Add post_deploy script to clean up docker stuff

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,15 @@
 set -euo pipefail
 
 DOCKER_COMPOSE_FILE=/var/www/bird_signs/docker-compose.prod.yml
+PORT=8080
 
 sudo chmod 600 ~/.ssh/deploy_rsa
 sudo chmod 755 ~/.ssh
 scp -i ~/.ssh/deploy_rsa docker-compose.prod.yml ${DEPLOY_USER}@${IP_ADDRESS}:${DOCKER_COMPOSE_FILE}
 
 ssh -i ~/.ssh/deploy_rsa ${DEPLOY_USER}@${IP_ADDRESS} "docker pull cfranklin11/tipresias_afl_data:latest \
-  && docker-compose -f ${DOCKER_COMPOSE_FILE} up -d --build"
+  && docker-compose -f ${DOCKER_COMPOSE_FILE} up -d"
+
+./scripts/wait-for-it.sh ${IP_ADDRESS}:${PORT} \
+  -t 60 \
+  -- ./scripts/post_deploy.sh
