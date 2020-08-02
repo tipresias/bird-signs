8c0f70a0cb25ab48c2eec834259914563c886eb1
Craig Franklin <craigjfranklin@gmail.com>
2020-08-03 08:47:49 +1000

    Normalise column names

--- afl_data/R/betting-odds.R
+++ afl_data/R/betting-odds.R
@@ -129,5 +129,8 @@ scrape_betting_odds <- function(splash_host) {
   betting_data <- .clean_betting_data(raw_betting_data)
   fixture_data <- .fetch_fixture_data()
 
-  .append_match_cols_to_betting_data(betting_data, fixture_data)
+  .append_match_cols_to_betting_data(betting_data, fixture_data) %>%
+    dplyr::rename_all(
+      ~ stringr::str_to_lower(.) %>% stringr::str_replace_all(., "\\.", "_")
+    )
 }
