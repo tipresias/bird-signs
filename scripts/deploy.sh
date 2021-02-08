242c5e5a2e415623b5a56b82a15dc27c67f78a9e
Craig Franklin <craigjfranklin@gmail.com>
2021-02-09 10:45:36 +1100

    Fix deploy script by making it match augury's

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,10 +12,13 @@ GCR_TOKEN=${GCR_TOKEN},\
 R_ENV=production,\
 "
 
-gcloud beta run deploy bird-signs \
+gcloud run deploy bird-signs \
+  --quiet \
   --image gcr.io/${PROJECT_ID}/bird-signs \
   --memory 4Gi \
+  --timeout 900 \
   --region australia-southeast1 \
-  --platform managed \
   --max-instances 5 \
+  --concurrency 1 \
+  --platform managed \
   --update-env-vars ${GOOGLE_ENV_VARS}
