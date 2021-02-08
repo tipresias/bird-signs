2396597c6802452adba31fb03a2103e653af08f9
Craig Franklin <craigjfranklin@gmail.com>
2021-02-08 20:52:19 +1100

    Add missing tests for betting & match data

--- afl_data/tests/testthat/test-betting-odds.R
+++ afl_data/tests/testthat/test-betting-odds.R
@@ -1,13 +1,27 @@
 describe("fetch_betting_odds()", {
-  describe("when data is empty", {
-    today <- lubridate::today()
-    future_start_date <- today + lubridate::years(1)
-    future_end_date <- today + lubridate::years(2)
-    future_data <- fetch_betting_odds(future_start_date, future_end_date)
+  today <- lubridate::today()
 
-    it("returns data from latest season with data available", {
-      expect_true("data.frame" %in% class(future_data))
-      expect_equal(nrow(future_data), 0)
+  describe("when data is empty", {
+    start_date <- today + lubridate::years(1)
+    end_date <- today + lubridate::years(2)
+    data <- fetch_betting_odds(start_date, end_date)
+
+    it("returns an empty data frame", {
+      expect_true("data.frame" %in% class(data))
+      expect_equal(nrow(data), 0)
+    })
+  })
+
+  describe("when some seasons have betting data but others don't", {
+    it("returns available betting data", {
+      # We can use hard-coded dates, because betting data stopped
+      # getting populated around mid-2020
+      start_date <- '2020-01-01'
+      end_date <- '2021-12-31'
+      data <- fetch_betting_odds(start_date, end_date)
+
+      expect_true("data.frame" %in% class(data))
+      expect_gt(nrow(data), 0)
     })
   })
 })
